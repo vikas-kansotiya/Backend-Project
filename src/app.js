ac241ba7cdef2a0c0e285a88dd4d25256873db6d
vikas-kansotiya <139054277+vikas-kansotiya@users.noreply.github.com>
2024-07-07 12:19:17 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -2,18 +2,24 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+//routes import
+import {router as userRouter} from './routes/user.routes.js';
 
-app.use(express.static("public"))
-app.use(cookieParser())
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
-export { app }
+//https://localhost:8000/api/v1/users/register
+
+export { app };
